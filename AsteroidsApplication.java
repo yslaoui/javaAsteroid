f4082d26f8e6b39cbca47ee9d10181921165963a
yslaoui <yslaouiyassine@gmail.com>
2024-07-08 16:50:18 +0100

    Creating the game window

--- AsteroidsApplication.java
+++ AsteroidsApplication.java
@@ -1,5 +1,24 @@
 package asteroid;
 
-public class AsteroidsApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class AsteroidsApplication extends Application {
+
+    @Override
+    public void start(Stage window) throws Exception {
+        Pane pane = new Pane();
+        pane.getChildren().add(new Circle(30,50,10));
+        Scene scene = new Scene(pane, 600,400);
+        window.setScene(scene);
+        window.show();
+    }
+
+    public static void main(String[] args) {
+        launch(AsteroidsApplication.class);
+    }
 
 }
