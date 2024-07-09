986e90087bd53021202a84dac38b5a12c3e04216
yslaoui <yslaouiyassine@gmail.com>
2024-07-09 16:33:23 +0100

    Moving the ship, changingits direction and acceleration. refactoring code into a Ship class

--- AsteroidsApplication.java
+++ AsteroidsApplication.java
@@ -2,6 +2,7 @@ package asteroid;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
@@ -18,27 +19,34 @@ public class AsteroidsApplication extends Application {
     public void start(Stage window) throws Exception {
         Pane pane = new Pane();
         pane.setPrefSize(600,400);
-        Polygon ship = new Polygon(-5,-5,10,0,-5,5);
-        ship.setTranslateX(200);
-        ship.setTranslateY(200);
+        Ship ship = new Ship(200,200);
+//        Polygon ship = new Polygon(-5,-5,10,0,-5,5);
+//        ship.setTranslateX(200);
+//        ship.setTranslateY(200);
+//
 
         Scene scene = new Scene(pane);
         // map of keyboard key statuses
         Map<KeyCode, Boolean> keyBoard = new HashMap<>();
         scene.setOnKeyPressed(event->keyBoard.put(event.getCode(), true));
         scene.setOnKeyReleased(event->keyBoard.put(event.getCode(), false));
-        pane.getChildren().add(ship);
-
+        pane.getChildren().add(ship.getCharacter());
+        Point2D movement = new Point2D(0,1);
         // animation timer that rotates the ship based on the keyboard status hashmap
         new AnimationTimer() {
             @Override
             public void handle(long l) {
                 if (keyBoard.getOrDefault(KeyCode.RIGHT, false)) {
-                    ship.setRotate(ship.getRotate() + 5);
+                    ship.turnRight();
                 }
                 if (keyBoard.getOrDefault(KeyCode.LEFT, false)) {
-                    ship.setRotate(ship.getRotate() - 5);
+                    ship.turnLeft();
                 }
+                if (keyBoard.getOrDefault(KeyCode.UP, false)) {
+                    ship.accelerate();
+                }
+                ship.move();
+
             }
         }.start();
 
@@ -51,3 +59,6 @@ public class AsteroidsApplication extends Application {
     }
 
 }
+
+
+
