ba5c3dc500b17ec5d9baed24f8d59ef1c3cb8de2
yslaoui <yslaouiyassine@gmail.com>
2024-07-11 17:20:05 +0100

    Asteroids with random shapes and movement

--- Asteroid.java
+++ Asteroid.java
@@ -2,8 +2,28 @@ package asteroid;
 
 import javafx.scene.shape.Polygon;
 
+import java.util.Random;
+
 public class Asteroid extends Character {
+    Random rnd;
+
     public Asteroid(double x, double y) {
         super(new PolygonFactory().createPolygon(), x,y);
+        // Setting initial random rotation
+        this.rnd = new Random();
+        super.getCharacter().setRotate(this.rnd.nextDouble(360));
+        // calling the accelerate() method a random number of times between 1 and 10.
+        int numberOfAccelerations = 1 + rnd.nextInt(10);
+        for (int i=0; i< numberOfAccelerations; i++) {
+            super.accelerate();
+        }
+    }
+
+    // Add a small rotation whenever an asteroid moves
+    @Override
+    public void move() {
+        super.move();
+        double moveRotation = 0.5 - this.rnd.nextDouble();
+        super.getCharacter().setRotate(super.getCharacter().getRotate() + moveRotation);
     }
 }
