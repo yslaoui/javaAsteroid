70e4f2f066fef2b0f282b2f384b656d673ca2ac6
yslaoui <yslaouiyassine@gmail.com>
2024-07-11 16:49:47 +0100

    Creating asteroids of random shapes

--- Asteroid.java
+++ Asteroid.java
@@ -4,6 +4,6 @@ import javafx.scene.shape.Polygon;
 
 public class Asteroid extends Character {
     public Asteroid(double x, double y) {
-        super(new Polygon(20,20,40,20,40,40, 20,40), x,y);
+        super(new PolygonFactory().createPolygon(), x,y);
     }
 }
--- AsteroidsApplication.java
+++ AsteroidsApplication.java
@@ -72,3 +72,7 @@ public class AsteroidsApplication extends Application {
     }
 }
 
+
+
+
+
